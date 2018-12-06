da14ed71a9ec3004768f74fb3a9b2bf4b97b568a
netcon <netcon@live.com>
2018-12-06 15:17:42 +0800

    update cli

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const client = require('hallu-client');
+const server = require('hallu-server');
+
+program
+    .option('-p, --ports <ports>', 'control,mock,proxy', '5260,5261,5262')
+    .parse(process.argv);
+
+const [cp, mp, pp] = program.ports.split(',').map(i => +i);
+
+client.listen(cp || 5260);
+server.listen(mp || 5261);
