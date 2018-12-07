bd1c3ae5714c8248ce159fb8fa01c94bd68bfc3d
netcon <netcon@live.com>
2018-12-08 04:15:33 +0800

    update cli

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ const [cp, mp, pp] = program.ports.split(',').map(i => +i);
 
 client.listen(cp || 5260);
 server.listen(mp || 5261);
+
+console.log('Frontend Host: http://0.0.0.0:' + cp);
+console.log('Mock Server Host: http://0.0.0.0:' + mp);
